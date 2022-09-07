d9427c658e9145b76cf8514969c3c8367806f903
Developer <developer@email.com>
2022-09-07 15:10:25 +0530

    Added a Room Dropdown for Select Room Filter in calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -5,6 +5,7 @@ import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import moment from 'moment';
 import hotelData from '../../json-data/hotelData.json';
+import RoomDropdown from './RoomDropdown';
 
 // const calendarEvents = [
 //     { title: 'event 1', date: '2022-09-01' },
@@ -13,9 +14,12 @@ import hotelData from '../../json-data/hotelData.json';
 
 function Calendar() {
     const [calendarEvents, setCalendarEvents] = useState([]);
+    const [rooms, setRooms] = useState([]);
+    const [room, setRoom] = useState([]);
 
     const handleDates = (rangeInfo) => {
         const calendarEventsData = [];
+        const roomData = [];
         const object = hotelData.prices.data;
 
         // convert object to key's array
@@ -29,9 +33,12 @@ function Calendar() {
                 let item = object[key];
                 // convert object to key's array
                 const itemObjKeys = Object.keys(object[key]);
-
                 // iterate over inner object
                 itemObjKeys.forEach((objKey, objIndex) => {
+                    if (objKey !== 'property') {
+                        roomData.push(objKey)
+                    }
+
                     if (!item[objKey].error && item[objKey].price) {
                         calendarEventsData.push({ title: `Room #${objKey} rate $${item[objKey].price}`, date: key })
                     }
@@ -40,22 +47,32 @@ function Calendar() {
             }
         });
 
+        setRooms([...new Set(roomData)])
         setCalendarEvents(calendarEventsData);//SET CALENDAR DATA
     }
 
+    const handleRoomFilter = (event) => {
+        setRoom(event.target.value);
+        console.log("Called Room Filter", event.target.value);
+    }
+
     return (
-        <FullCalendar
-            plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            weekends={true}
-            events={calendarEvents}
-            datesSet={handleDates}
-        />
+        <>
+            <RoomDropdown roomsData={rooms} handleRoomFilterFunc={handleRoomFilter} selectedRoom={room} />
+            <FullCalendar
+                plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                    left: "prev,next today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+                weekends={true}
+                events={calendarEvents}
+                datesSet={handleDates}
+            />
+        </>
+
     )
 }
 
---
+++ src/components/calendar/RoomDropdown.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+
+function RoomDropdown({ roomsData, handleRoomFilterFunc, selectedRoom = "" }) {
+    return (
+        <Box sx={{ minWidth: 120, mb: 3 }}>
+            <FormControl sx={{ minWidth: 120 }}>
+                <InputLabel id="room-dropdown-lbl">Room</InputLabel>
+                <Select
+                    labelId="room-dropdown-lbl"
+                    id="room-dropdown"
+                    name="room-dropdown"
+                    value={selectedRoom}
+                    label="Room"
+                    onChange={handleRoomFilterFunc}
+                >
+                    <MenuItem value="">Select Room</MenuItem>
+                    {
+                        roomsData.map((room) => {
+                            return <MenuItem value={room} key={room}>{room}</MenuItem>
+                        })
+                    }
+                </Select>
+            </FormControl>
+        </Box>
+    )
+}
+
+export default RoomDropdown;
