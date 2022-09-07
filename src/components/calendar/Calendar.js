7df94321f488d5b1aad6d26cd0a81409b9626456
Developer <developer@email.com>
2022-09-07 11:38:55 +0530

    Added Dynamically Handled Calendar Data for Room Rates.

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import moment from 'moment';
+import hotelData from '../../json-data/hotelData.json';
+
+// const calendarEvents = [
+//     { title: 'event 1', date: '2022-09-01' },
+//     { title: 'event 2', date: '2022-09-02' }
+// ];
 
 function Calendar() {
+    const [calendarEvents, setCalendarEvents] = useState([]);
+
+    const handleDates = (rangeInfo) => {
+        let calendarData = [];
+        const calendarEventsData = [];
+        const object = hotelData.prices.data;
+        for (const property in object) {
+            if (property >= moment(rangeInfo.start).format('YYYY-MM-DD')
+                && property < moment(rangeInfo.end).format('YYYY-MM-DD')) {
+                calendarData.push(object[property]);
+                let item = object[property];
+                for (const pInner in item) {
+                    if (!item[pInner].error && item[pInner].price) {
+                        calendarEventsData.push({ title: `Room #${pInner} rate $${item[pInner].price}`, date: property })
+                    }
+                }
+
+            }
+        }
+
+        setCalendarEvents(calendarEventsData);//SET CALENDAR DATA
+    }
+
     return (
         <FullCalendar
             plugins={[timeGridPlugin, dayGridPlugin, interactionPlugin]}
-            initialView="timeGridWeek"
+            initialView="dayGridMonth"
             headerToolbar={{
                 left: "prev,next today",
                 center: "title",
                 right: "dayGridMonth,timeGridWeek,timeGridDay",
             }}
             weekends={true}
-            events={[
-                { title: 'event 1', date: '2022-09-01' },
-                { title: 'event 2', date: '2022-09-02' }
-            ]}
+            events={calendarEvents}
+            datesSet={handleDates}
         />
     )
 }
