5440c27d166a4c3f7c6ab30c28cf691409c15418
Developer <developer@email.com>
2022-09-07 17:34:36 +0530

    Filter Dropdown with functionality for show calendar data added.

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -6,6 +6,7 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import moment from 'moment';
 import hotelData from '../../json-data/hotelData.json';
 import RoomDropdown from './RoomDropdown';
+import { useEffect } from 'react';
 
 // const calendarEvents = [
 //     { title: 'event 1', date: '2022-09-01' },
@@ -16,44 +17,64 @@ function Calendar() {
     const [calendarEvents, setCalendarEvents] = useState([]);
     const [rooms, setRooms] = useState([]);
     const [room, setRoom] = useState([]);
+    const [startDate, setStartDate] = useState("");
+    const [endDate, setEndDate] = useState("");
+    let calendarEventsData = [];
 
     const handleDates = (rangeInfo) => {
-        const calendarEventsData = [];
-        const roomData = [];
+        setStartDate(rangeInfo.start);
+        setEndDate(rangeInfo.end);
+    }
+
+    const filterData = () => {
         const object = hotelData.prices.data;
+        calendarEventsData = [];//Reset
 
         // convert object to key's array
         const objectKeys = Object.keys(object);
 
         // iterate over object
         objectKeys.forEach((key, index) => {
-            if (key >= moment(rangeInfo.start).format('YYYY-MM-DD')
-                && key < moment(rangeInfo.end).format('YYYY-MM-DD')
+            if (key >= moment(startDate).format('YYYY-MM-DD')
+                && key < moment(endDate).format('YYYY-MM-DD')
             ) {
                 let item = object[key];
                 // convert object to key's array
-                const itemObjKeys = Object.keys(object[key]);
-                // iterate over inner object
-                itemObjKeys.forEach((objKey, objIndex) => {
-                    if (objKey !== 'property') {
-                        roomData.push(objKey)
-                    }
-
-                    if (!item[objKey].error && item[objKey].price) {
-                        calendarEventsData.push({ title: `Room #${objKey} rate $${item[objKey].price}`, date: key })
-                    }
-                });
-
+                const itemObjKeys = (room && (room.length > 0)) ? [room] : Object.keys(object[key]);
+                setRoomData(itemObjKeys, item, key, (room && (room.length > 0)) ? true : false);
             }
         });
 
-        setRooms([...new Set(roomData)])
+
+
+    }
+
+    const setRoomData = (itemObjKeys, item, key, filterStatus) => {
+        const roomData = [];
+        // iterate over inner object
+        itemObjKeys.forEach((objKey, objIndex) => {
+            if (objKey !== 'property' && !filterStatus) {
+                roomData.push(objKey)
+            }
+
+            if (!item[objKey].error && item[objKey].price) {
+                calendarEventsData.push({ title: `Room #${objKey} rate $${item[objKey].price}`, date: key })
+            }
+        });
+
+        if (!filterStatus) {
+            setRooms([...new Set(roomData)]);
+        }
+
         setCalendarEvents(calendarEventsData);//SET CALENDAR DATA
     }
 
+    useEffect(() => {
+        filterData();
+    }, [startDate, endDate, room])
+
     const handleRoomFilter = (event) => {
         setRoom(event.target.value);
-        console.log("Called Room Filter", event.target.value);
     }
 
     return (
